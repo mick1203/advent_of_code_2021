65770567549d8aa17fb61b2f6966f5b9bad49abb
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-06 23:41:28 +0100

    DAY-06: Add basic structure and input files

---
+++ day-06-lanternfish/src/main/java/com/michaelburgstaller/adventofcode/lanternfish/Lanternfish.java
@@ -0,0 +1,10 @@
+package com.michaelburgstaller.adventofcode.lanternfish;
+
+import com.michaelburgstaller.adventofcode.common.Exercise;
+
+public class Lanternfish extends Exercise {
+
+    public static void main(String[] args) {
+    }
+
+}
