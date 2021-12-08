62615b47d78aed8c0b1a82f85d99c8bfd1de6bdc
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-08 10:50:54 +0100

    DAY-08: Add decode method to Pattern class

--- day-08-seven-segment-search/src/main/java/com/michaelburgstaller/adventofcode/sevensegmentsearch/SevenSegmentSearch.java
+++ day-08-seven-segment-search/src/main/java/com/michaelburgstaller/adventofcode/sevensegmentsearch/SevenSegmentSearch.java
@@ -5,6 +5,7 @@ import com.michaelburgstaller.adventofcode.common.Exercise;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SevenSegmentSearch extends Exercise {
 
@@ -22,14 +23,34 @@ public class SevenSegmentSearch extends Exercise {
     );
 
     private static class Pattern {
-        public String value;
+        public String pattern;
+        public Integer value;
 
-        public Pattern(String value) {
-            this.value = value;
+        public Pattern(String pattern) {
+            this.pattern = pattern;
+            this.value = null;
+        }
+
+        public void decode() {
+            switch (pattern.length()) {
+                case 2: value = 1; break;
+                case 3: value = 7; break;
+                case 4: value = 4; break;
+                case 7: value = 8; break;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Pattern{" +
+                    "pattern='" + pattern + '\'' +
+                    ", value=" + value +
+                    '}';
         }
 
         public static Pattern parse(String rawValue) {
-            var value = rawValue.strip();
+            var valueTokens = rawValue.strip().split("");
+            var value = Arrays.stream(valueTokens).sorted().collect(Collectors.joining(""));
             return new Pattern(value);
         }
     }
@@ -53,12 +74,8 @@ public class SevenSegmentSearch extends Exercise {
 
     private static void countOccurrencesOfOneFourSevenAndEight(List<Entry> entries) {
         var occurrences = entries.stream()
-                .flatMap(entry -> entry.output.stream())
-                .filter(output ->
-                        output.value.length() == 2 // 1
-                        || output.value.length() == 3 // 7
-                        || output.value.length() == 4 // 4
-                        || output.value.length() == 7 // 8
+                .flatMap(entry -> entry.output.stream().peek(Pattern::decode))
+                .filter(output -> output.value != null
                 ).count();
 
         System.out.println("There are a total of '" + occurrences + "' 1s, 4s, 7s, and 8s in the input");
