501fb10fdfdc038827e9db24803491f2bfe666c8
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-08 11:11:19 +0100

    DAY-08: Setup decoding flow

--- day-08-seven-segment-search/src/main/java/com/michaelburgstaller/adventofcode/sevensegmentsearch/SevenSegmentSearch.java
+++ day-08-seven-segment-search/src/main/java/com/michaelburgstaller/adventofcode/sevensegmentsearch/SevenSegmentSearch.java
@@ -3,6 +3,7 @@ package com.michaelburgstaller.adventofcode.sevensegmentsearch;
 import com.michaelburgstaller.adventofcode.common.Exercise;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,6 +23,31 @@ public class SevenSegmentSearch extends Exercise {
             "abcdfg", 9
     );
 
+    private static class Output {
+        public String pattern;
+        public Integer value;
+
+        public Output(String pattern) {
+            this.pattern = pattern;
+            this.value = null;
+        }
+
+        public void decode(Map<String, Integer> decodingTable) {
+            value = decodingTable.get(pattern);
+        }
+
+        @Override
+        public String toString() {
+            return pattern + '(' + value + ')';
+        }
+
+        public static Output parse(String rawValue) {
+            var outputTokens = rawValue.strip().split("");
+            var pattern = Arrays.stream(outputTokens).sorted().collect(Collectors.joining(""));
+            return new Output(pattern);
+        }
+    }
+
     private static class Pattern {
         public String pattern;
         public Integer value;
@@ -33,19 +59,28 @@ public class SevenSegmentSearch extends Exercise {
 
         public void decode() {
             switch (pattern.length()) {
-                case 2: value = 1; break;
-                case 3: value = 7; break;
-                case 4: value = 4; break;
-                case 7: value = 8; break;
+                case 2:
+                    value = 1;
+                    break;
+                case 3:
+                    value = 7;
+                    break;
+                case 4:
+                    value = 4;
+                    break;
+                case 5:
+                    break; // 2, 3, 5
+                case 6:
+                    break; // 0, 6, 9
+                case 7:
+                    value = 8;
+                    break;
             }
         }
 
         @Override
         public String toString() {
-            return "Pattern{" +
-                    "pattern='" + pattern + '\'' +
-                    ", value=" + value +
-                    '}';
+            return pattern + '(' + value + ')';
         }
 
         public static Pattern parse(String rawValue) {
@@ -57,24 +92,41 @@ public class SevenSegmentSearch extends Exercise {
 
     private static class Entry {
         public List<Pattern> patterns;
-        public List<Pattern> output;
+        public List<Output> outputs;
+        public Map<String, Integer> decodingTable;
 
-        public Entry(List<Pattern> patterns, List<Pattern> output) {
+        public Entry(List<Pattern> patterns, List<Output> outputs) {
             this.patterns = patterns;
-            this.output = output;
+            this.outputs = outputs;
+            this.decodingTable = new HashMap<>();
+        }
+
+        public void decode() {
+            patterns.forEach(Pattern::decode);
+
+            for (var pattern : patterns) {
+                decodingTable.put(pattern.pattern, pattern.value);
+            }
+
+            outputs.forEach(output -> output.decode(decodingTable));
+        }
+
+        @Override
+        public String toString() {
+            return "Entry: [" + patterns + " | " + outputs + ']';
         }
 
         public static Entry parse(String rawValue) {
             var entryTokens = rawValue.split("\\|");
-            var patterns = Arrays.stream(entryTokens[0].split(" ")).map(Pattern::parse).toList();
-            var outputs = Arrays.stream(entryTokens[1].split(" ")).map(Pattern::parse).toList();
+            var patterns = Arrays.stream(entryTokens[0].strip().split(" ")).map(Pattern::parse).toList();
+            var outputs = Arrays.stream(entryTokens[1].strip().split(" ")).map(Output::parse).toList();
             return new Entry(patterns, outputs);
         }
     }
 
     private static void countOccurrencesOfOneFourSevenAndEight(List<Entry> entries) {
         var occurrences = entries.stream()
-                .flatMap(entry -> entry.output.stream().peek(Pattern::decode))
+                .flatMap(entry -> entry.outputs.stream())
                 .filter(output -> output.value != null
                 ).count();
 
@@ -82,9 +134,12 @@ public class SevenSegmentSearch extends Exercise {
     }
 
     public static void main(String[] args) {
-        var lines = getLineStream().map(Entry::parse).toList();
+        var entries = getLineStream().map(Entry::parse).toList();
+        entries.forEach(Entry::decode);
 
-        countOccurrencesOfOneFourSevenAndEight(lines);
+        countOccurrencesOfOneFourSevenAndEight(entries);
+
+        entries.forEach(System.out::println);
     }
 
 }
