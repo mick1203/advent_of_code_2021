57acb5834bae4b1c122b193f997564289bee72b1
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-09 18:50:51 +0100

    DAY-09: Add solution for finding largest basins

--- day-09-smoke-basin/src/main/java/com/michaelburgstaller/adventofcode/smokebasin/SmokeBasin.java
+++ day-09-smoke-basin/src/main/java/com/michaelburgstaller/adventofcode/smokebasin/SmokeBasin.java
@@ -3,6 +3,7 @@ package com.michaelburgstaller.adventofcode.smokebasin;
 import com.michaelburgstaller.adventofcode.common.Exercise;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -42,13 +43,44 @@ public class SmokeBasin extends Exercise {
         }
     }
 
-    private static Boolean isLowest(Integer currentPosition, List<Integer> neighbors) {
-        for (var neighbor : neighbors) {
-            if (neighbor <= currentPosition) {
-                return false;
+    private static class Basin {
+        public List<Tuple<Integer, Integer>> points;
+        public Floor floor;
+
+        public Basin(Floor floor, Tuple<Integer, Integer> lowestPoint) {
+            this.points = new ArrayList<>();
+            points.add(lowestPoint);
+            this.floor = floor;
+        }
+
+        public void expand() {
+            var shouldExpand = true;
+            var expandedBasin = new ArrayList<Tuple<Integer, Integer>>();
+            while (shouldExpand) {
+                for (var point : points) {
+                    if (!containsPoint(expandedBasin, point)) expandedBasin.add(point);
+                    for (var neighbor : getNeighborCoordinates(point, floor.grid.length, floor.grid[0].length)) {
+                        if (!containsPoint(expandedBasin, neighbor) && floor.grid[neighbor.left][neighbor.right] != 9) {
+                            expandedBasin.add(neighbor);
+                        }
+                    }
+                }
+
+                shouldExpand = points.size() != expandedBasin.size();
+                points = expandedBasin;
+                expandedBasin = new ArrayList();
             }
         }
-        return true;
+
+        public Integer getSize() {
+            return points.size();
+        }
+    }
+
+    public static Boolean containsPoint(List<Tuple<Integer, Integer>> list, Tuple<Integer, Integer> point) {
+        return list.stream()
+                .filter(p -> p.left.compareTo(point.left) == 0 && p.right.compareTo(point.right) == 0)
+                .count() > 0;
     }
 
     private static List<Tuple<Integer, Integer>> getNeighborCoordinates(Tuple<Integer, Integer> current, Integer height, Integer width) {
@@ -73,8 +105,18 @@ public class SmokeBasin extends Exercise {
         return coordinates;
     }
 
-    private static void findSumOfRiskLevelForLowestPointsOnFloor(Floor floor) {
-        var sumOfRisk = 0;
+    private static Boolean isLowest(Integer currentPosition, List<Integer> neighbors) {
+        for (var neighbor : neighbors) {
+            if (neighbor <= currentPosition) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Tuple<Integer, Integer>> findLowestPoints(Floor floor) {
+        var lowestPoints = new ArrayList<Tuple<Integer, Integer>>();
+
         for (var row = 0; row < floor.grid.length; row++) {
             for (var column = 0; column < floor.grid[0].length; column++) {
                 var current = floor.grid[row][column];
@@ -83,18 +125,53 @@ public class SmokeBasin extends Exercise {
                         .toList();
 
                 if (isLowest(current, neighbors)) {
-                    sumOfRisk += 1 + current;
+                    lowestPoints.add(new Tuple<>(row, column));
                 }
             }
+        }
+
+        return lowestPoints;
+    }
+
+    private static void findSumOfRiskLevelForLowestPointsOnFloor(Floor floor) {
+        var lowestPoints = findLowestPoints(floor);
+
+        var sumOfRisk = 0;
+        for (var lowestPoint : lowestPoints) {
+            sumOfRisk += 1 + floor.grid[lowestPoint.left][lowestPoint.right];
         }
 
         System.out.println("The sum of the risk levels of the lowest points on this floor is '" + sumOfRisk + "'");
     }
 
+    private static void calculateProductOfLargestBasins(Floor floor, Integer number) {
+        var lowestPoints = findLowestPoints(floor);
+
+        var basins = new ArrayList<Basin>();
+        for (var lowestPoint : lowestPoints) {
+            basins.add(new Basin(floor, lowestPoint));
+        }
+
+        for (var basin : basins) {
+            basin.expand();
+        }
+
+        basins.sort(Comparator.comparingInt(Basin::getSize).reversed());
+
+        var product = 1;
+        for (var i = 0; i < number; i++) {
+            product *= basins.get(i).getSize();
+        }
+
+        System.out.println("The product of the '" + number + "' largest basins is '" + product + "'");
+    }
+
+
     public static void main(String[] args) {
         var floor = Floor.parse(getLineStream());
 
         findSumOfRiskLevelForLowestPointsOnFloor(floor);
+        calculateProductOfLargestBasins(floor, 3);
     }
 
 }
